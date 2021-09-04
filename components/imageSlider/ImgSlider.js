5974728d41e79597ab6af2c5fcb06c65d370d4a1
Saad Siddiqui <saadsiddiqui927@gmail.com>
2021-09-04 18:19:18 +0530

    Added image slider comp

---
+++ components/imageSlider/ImgSlider.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 100vh;
+`;
+
+const ImgSlider = () => {
+  return (
+    <Container>
+      <h1>Image slider</h1>
+    </Container>
+  );
+};
+
+export default ImgSlider;
